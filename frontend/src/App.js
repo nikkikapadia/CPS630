db18e1bdc18c6c25813cfed54f9dcd9f8dd30106
nikkikapadia <nikki.kapadia7@gmail.com>
2024-03-07 20:40:15 -0500

    added routing and 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./nav";
 import { HomePage } from "./home/Home";
+import Page404 from "./404/404";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <HomePage />
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
